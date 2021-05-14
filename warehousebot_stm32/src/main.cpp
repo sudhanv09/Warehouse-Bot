92ffe7ad682f142e7e2644d0844201007f722222
Sudhanv <mystogun125@gmail.com>
2021-05-14 21:11:13 +0530

    final changes

--- warehousebot_stm32/src/main.cpp
+++ warehousebot_stm32/src/main.cpp
@@ -2,36 +2,32 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h>
-#include <actionlib_msgs/GoalStatusArray.h>
-#include <sensor_msgs/BatteryState.h>
 
-#define IN1 D9
-#define IN2 D10
-#define EN1 D11
-// change pins
-#define IN3 D8
-#define IN4 D7
-#define EN2 D4
-
-#define ENCA_1 D3
-#define ENCB_1 D2
-// change pins
-#define ENCA_2 D1
-#define ENCB_2 D0
+// left motor - 1
+#define IN1 PB3
+#define IN2 PB5
+#define EN1 PB14
+// right motor - 2
+#define IN3 PB4
+#define IN4 PB10
+#define EN2 PB13
+// encoder pins
+#define ENCA_1 PA5
+#define ENCB_1 PA6
+#define ENCA_2 PA1
+#define ENCB_2 PA0
 
 int left_pos = 0;
 int right_pos = 0;
 int lmotor_pwm = 0;
 int rmotor_pwm = 0;
-int robot_state;
-
+int state;
 
 ros::NodeHandle nh;
 std_msgs::Int16 leftencoder_msg;
 std_msgs::Int16 rightencoder_msg;
 std_msgs::Float32 lmotor_cmd;
 std_msgs::Float32 rmotor_cmd;
-actionlib_msgs::GoalStatusArray robot_status;
 
 void lmotor_cmd_cb(const std_msgs::Float32 &cmd)
 {
@@ -41,11 +37,6 @@ void lmotor_cmd_cb(const std_msgs::Float32 &cmd)
 void rmotor_cmd_cb(const std_msgs::Float32 &cmd)
 {
   rmotor_pwm = cmd.data;
-}
-
-void getGoalStatus(const actionlib_msgs::GoalStatusArray &cmd)
-{
-  robot_state = cmd.status_list->status;
 }
 
 void readLeftEncoder()
@@ -74,39 +65,84 @@ void readRightEncoder()
   }
 }
 
-void runMotor(int lmotor_pwm, int rmotor_pwm)
+void runMotor()
 {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(EN1, lmotor_pwm);
-  delay(10);
+  if (lmotor_pwm > 0)
+  {
+    if (rmotor_pwm > 0)
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(EN1, lmotor_pwm);
+      // delay(10);
 
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(EN2, rmotor_pwm);
-  delay(10);
-}
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      analogWrite(EN2, rmotor_pwm);
+    }
+    else
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(EN1, lmotor_pwm);
+      // delay(10);
 
-void scissorMotor()
-{
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      analogWrite(EN2, rmotor_pwm);
+    }
+  }
 
-}
+  else
+  {
+    if (rmotor_pwm > 0)
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      analogWrite(EN1, lmotor_pwm);
+      // delay(10);
 
-void gripper()
-{
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      analogWrite(EN2, rmotor_pwm);
+    }
 
+    else
+    {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      analogWrite(EN1, lmotor_pwm);
+      // delay(10);
+
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      analogWrite(EN2, rmotor_pwm);
+    }
+  }
 }
 
 ros::Publisher lencoder_adv("left_wheel", &leftencoder_msg);
 ros::Publisher rencoder_adv("right_wheel", &rightencoder_msg);
 ros::Subscriber<std_msgs::Float32> lcmd_in("lmotor_cmd", &lmotor_cmd_cb);
 ros::Subscriber<std_msgs::Float32> rcmd_in("rmotor_cmd", &rmotor_cmd_cb);
-ros::Subscriber<actionlib_msgs::GoalStatusArray> robot_state("/move_base/status", &getGoalStatus);
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinInit();
+  pinMode(ENCA_1, INPUT);
+  pinMode(ENCB_1, INPUT);
+  pinMode(ENCA_2, INPUT);
+  pinMode(ENCB_2, INPUT);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(EN1, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(EN2, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(ENCA_1), readLeftEncoder, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCA_2), readRightEncoder, RISING);
 
   nh.initNode();
   nh.advertise(lencoder_adv);
@@ -121,35 +157,11 @@ void loop()
   // put your main code here, to run repeatedly:
   leftencoder_msg.data = left_pos;
   lencoder_adv.publish(&leftencoder_msg);
+
   rightencoder_msg.data = right_pos;
   rencoder_adv.publish(&rightencoder_msg);
 
-  runMotor(lmotor_pwm, rmotor_pwm);
-
-  if (robot_state == 3) 
-  {
-    
-  }
+  runMotor();
 
   nh.spinOnce();
 }
-
-void pinInit()
-{
-
-pinMode(ENCA_1, INPUT);
-pinMode(ENCB_1, INPUT);
-pinMode(ENCA_2, INPUT);
-pinMode(ENCB_2, INPUT);
-pinMode(IN1, OUTPUT);
-pinMode(IN2, OUTPUT);
-pinMode(EN1, OUTPUT);
-pinMode(IN3, OUTPUT);
-pinMode(IN4, OUTPUT);
-pinMode(EN2, OUTPUT);
-
-attachInterrupt(digitalPinToInterrupt(ENCA_1), readLeftEncoder, RISING);
-attachInterrupt(digitalPinToInterrupt(ENCA_2), readRightEncoder, RISING);
-}
-
-
